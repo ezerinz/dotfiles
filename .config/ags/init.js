44a212c0ad2ea42f01893714d374df008126e255
Edwin <edwinnnzx@gmail.com>
2024-04-19 15:02:26 +0800

    global brightness & update hyprland

--- .config/ags/init.js
+++ .config/ags/init.js
@@ -3,10 +3,12 @@ import { initTheme, initWallpaper, setWallpaper } from "./functions/theme.js";
 import monitor_configuration from "./functions/monitor_configuration.js";
 import applyAnimation from "./functions/apply_animation.js";
 import recorder from "./services/screen_record.js";
+import brightness from "./services/brightness.js";
 
 function globals() {
   globalThis.setWall = setWallpaper;
   globalThis.recorder = recorder;
+  globalThis.brightness = brightness;
 }
 
 export default function() {
